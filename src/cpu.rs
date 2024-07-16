8060013ca0c4fd562aa0b40e7bd542c2f4ddb339
Alex Shore <alex@shore.me.uk>
2024-07-16 23:32:45 +0100

    more instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -67,8 +67,8 @@ pub struct Cpu {
 }
 
 impl Cpu {
-    pub fn new() -> Self {
-        let mut res = Self {
+    pub fn new() -> Cpu {
+        let mut res = Cpu {
             pixels: vec![false; (SCREEN_HEIGHT * SCREEN_WIDTH) as usize],
             memory: vec![0; 4096],
             stack: vec![],
@@ -98,6 +98,11 @@ impl Cpu {
         self.execute(instruction, keys)
     }
 
+    pub fn decrement_timers(&mut self) {
+        self.sound_timer -= 1;
+        self.delay_timer -= 1;
+    }
+
     fn decode(&self) -> Instruction {
         Instruction::from(BigEndian::read_u16(&self.memory[self.pc..=self.pc + 1]))
     }
@@ -107,34 +112,34 @@ impl Cpu {
 
         match ins.op {
             0x0 => match ins.nn {
-                // clear screen
+                // Clear pixels
                 0xE0 => {
                     self.clear_pixels();
                     self.update_display = true
                 }
 
-                // return from subroutine
+                // Return
                 0xEE => self.pc = self.stack.pop().unwrap(),
 
                 _ => unimplemented!(),
             },
 
-            // jump to nnn
+            // PC = NNN
             0x1 => self.pc = ins.nnn as usize,
 
-            // call subroutine nnn
+            // Call Subroutine NNN
             0x2 => {
                 self.stack.push(self.pc);
                 self.pc = ins.nnn as usize
             }
 
-            // pc += 2 if Vx == nn
+            // PC = PC + 2 IF Vx == nn
             0x3 if self.v[ins.x] == ins.nn => self.pc += 2,
 
-            // pc += 2 if Vx != nn
+            // PC = PC + 2 IF Vx != nn
             0x4 if self.v[ins.x] != ins.nn => self.pc += 2,
 
-            // pc += 2 if Vx == Vy
+            // PC = PC + 2 IF Vx == Vy
             0x5 if ins.n == 0 => {
                 if self.v[ins.x] == self.v[ins.y] {
                     self.pc += 2
@@ -161,26 +166,43 @@ impl Cpu {
                 // Vx = Vx ^ Vy
                 0x3 => self.v[ins.x] ^= self.v[ins.y],
 
-                // Vx = Vx + Vy
+                // Vx = Vx + Vy (overflow in VF)
                 0x4 => {
-                    let mut res = self.v[ins.x] as u16 + self.v[ins.y] as u16;
-                    if res > 255 {
-                        res -= 256;
-                        self.v[0xF] = 1
-                    } else {
-                        self.v[0xF] = 0
-                    }
-                    self.v[ins.x] = res as u8
+                    let (res, overflow) = self.v[ins.x].overflowing_add(self.v[ins.y]);
+                    self.v[0xF] = overflow as u8;
+                    self.v[ins.x] = res;
                 }
 
-                0x5..=0x7 => todo!(),
+                // Vx = Vx - Vy (NOT overflow in VF)
+                0x5 => {
+                    let (res, overflow) = self.v[ins.x].overflowing_sub(self.v[ins.y]);
+                    self.v[0xF] = !overflow as u8;
+                    self.v[ins.x] = res;
+                }
 
-                0xE => todo!(),
+                // Vx = Vx >> 1 (overflow in VF)
+                0x6 => {
+                    self.v[0xF] = if self.v[ins.x] & 0b00000001 > 0 { 1 } else { 0 };
+                    self.v[ins.x] >>= 1;
+                }
+
+                // Vx = Vy - Vx (NOT overflow in VF)
+                0x7 => {
+                    let (res, overflow) = self.v[ins.y].overflowing_sub(self.v[ins.x]);
+                    self.v[0xF] = !overflow as u8;
+                    self.v[ins.x] = res;
+                }
+
+                // Vx = Vx << 1 (overflow in VF)
+                0xE => {
+                    self.v[0xF] = if self.v[ins.x] & 0b10000000 > 0 { 1 } else { 0 };
+                    self.v[ins.x] <<= 1;
+                }
 
                 _ => unimplemented!(),
             },
 
-            // pc += 2 if Vx != Vy
+            // PC = PC + 2 IF Vx != Vy
             0x9 if ins.n == 0 => {
                 if self.v[ins.x] != self.v[ins.y] {
                     self.pc += 2
@@ -190,13 +212,13 @@ impl Cpu {
             // I = nnn
             0xA => self.i = ins.nnn,
 
-            // pc = nnn + V0
+            // PC = nnn + V0
             0xB => self.pc = (ins.nnn + self.v[0] as u16) as usize,
 
             // Vx = rand(0-255) & nn
             0xC => self.v[ins.x] = self.rng.gen_range(0..255u8) & ins.nn,
 
-            // draw things :3
+            // Draw Sprite from I at (Vx, Vy)
             0xD => {
                 self.draw_sprite(ins);
                 self.update_display = true;
@@ -204,11 +226,13 @@ impl Cpu {
 
             // keypress things :3
             0xE => match ins.nn {
+                // PC = PC + 2 IF keys[Vx] == True
                 0x9E => {
                     if keys[self.v[ins.x] as usize] {
                         self.pc += 2
                     }
                 }
+                // PC = PC + 2 IF keys[Vx] == False
                 0xA1 => {
                     if !keys[self.v[ins.x] as usize] {
                         self.pc += 2
@@ -218,9 +242,11 @@ impl Cpu {
             },
 
             0xF => match ins.nn {
+                // Vx = DT
                 0x07 => self.v[ins.x] = self.delay_timer,
+
+                // PAUSE until any keypressed then store in Vx
                 0x0A => {
-                    // PAUSE until any keypressed then store in Vx
                     if !keys.iter().any(|&key| key) {
                         self.pc -= 2
                     } else {
@@ -231,31 +257,41 @@ impl Cpu {
                         }
                     }
                 }
+
+                // DT = Vx
                 0x15 => self.delay_timer = self.v[ins.x],
+
+                // ST = Vx
                 0x18 => self.sound_timer = self.v[ins.x],
+
+                // I = I + Vx
                 0x1E => self.i += self.v[ins.x] as u16,
+
+                // I = font[Vx] memory location
                 0x29 => self.i = (0x50 + (self.v[ins.x] * 5)) as u16,
+
+                // memory[i..i + 2] = Vx BCD
                 0x33 => {
-                    // store BCD in memory[i..i + 2]
                     self.memory[self.i as usize] = self.v[ins.x] / 100;
                     self.memory[self.i as usize + 1] = self.v[ins.x] % 100 / 10;
                     self.memory[self.i as usize + 2] = self.v[ins.x] % 10
                 }
+
+                // memory[i..i + x] = V0..Vx
                 0x55 => {
-                    // store V0..Vx in memory[i..i + x]
                     for reg in 0..ins.x {
                         self.memory[self.i as usize + reg] = self.v[reg]
                     }
                 }
+
+                // V0..Vx = memory[i..i + x]
                 0x65 => {
-                    // read V0..Vx from memory[i..i + x]
                     for reg in 0..ins.x {
                         self.v[reg] = self.memory[self.i as usize + reg]
                     }
                 }
                 _ => unimplemented!(),
             },
-
             _ => unimplemented!(),
         }
     }
@@ -274,17 +310,18 @@ impl Cpu {
         self.v[0xF] = 0;
 
         for row in 0..height {
-            let sprite_row = self.memory[self.i as usize + row as usize];
+            let sprite_byte = self.memory[self.i as usize + row as usize];
 
             for col in 0..8u8 {
-                let sprite_pixel = !matches!(sprite_row & (0x80 >> col), 0);
+                // match current bit in byte
+                let sprite_bit = !matches!(sprite_byte & (0x80 >> col), 0);
 
                 let screen_pixel = &mut self.pixels
                     [((start_y + row) as u32 * SCREEN_WIDTH + (start_x + col) as u32) as usize];
 
                 // if pixel is on in the sprite
-                if sprite_pixel {
-                    match (&sprite_pixel, &screen_pixel) {
+                if sprite_bit {
+                    match (&sprite_bit, &screen_pixel) {
                         (true, true) => {
                             *screen_pixel = false;
                             self.v[0xF] = 1
